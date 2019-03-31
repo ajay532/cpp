45fba066a58a02ce74327336d496a025c6dcd2ad
ajay532 <ajaypandey1996@gmail.com>
2019-03-31 12:56:16 +0530

    HelloWorld added

---
+++ HelloWorld.cpp
@@ -0,0 +1,11 @@
+//https://hackerrank-challenge-pdfs.s3.amazonaws.com/8038-cpp-hello-world-English?AWSAccessKeyId=AKIAJ4WZFDFQTZRGO3QA&Expires=1554019813&Signature=Ru0V1SngnmyFv6Tg4VY5uMvdyMo%3D&response-content-disposition=inline%3B%20filename%3Dcpp-hello-world-English.pdf&response-content-type=application%2Fpdf
+
+// HelloWorld problem
+
+#include <iostream>
+using namespace std;
+int main(){
+cout<<"Hello, World!\n";
+return 0;
+}
+
